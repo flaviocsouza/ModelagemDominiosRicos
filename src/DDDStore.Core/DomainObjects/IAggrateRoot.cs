d192637921275648c747e05fb9e05bb43a3ebe9c
Flavio Augusto Corrêa de Souza <flavio.augusto.c.souza@gmail.com>
2023-08-07 22:44:29 -0400

    Adicionar arquivos de projeto.

---
+++ src/DDDStore.Core/DomainObjects/IAggrateRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDStore.Core.DomainObjects
+{
+    //Interface de Marcação
+    //Ela em si Não faz nada Mas  Serve para marcar as classes que são nesse caso Raizes de Agregação
+    public interface IAggrateRoot
+    {
+    }
+}
